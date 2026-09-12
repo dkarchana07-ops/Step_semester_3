387c423996bea0c9ec733805ac6e6c2759b17c58
dkarchana07-ops <dkarchana07@gmail.com>
2026-09-12 18:02:53 +0530

    Complete Week 2 assignment problems

---
+++ src/main/java/week2/assigment_problems/WordReversalEncoder.java
@@ -0,0 +1,25 @@
+package week2.assigment_problems;
+
+public class WordReversalEncoder {
+
+    public static String reverseEachWord(String sentence) {
+        String[] words = sentence.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            StringBuilder reversedWord = new StringBuilder(words[i]);
+            reversedWord.reverse();
+            result.append(reversedWord);
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String output = reverseEachWord("hello club");
+        System.out.println(output);
+    }
+}
