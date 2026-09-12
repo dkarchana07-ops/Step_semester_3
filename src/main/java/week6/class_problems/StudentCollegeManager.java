5acae872abb781a0be6950a9148135255a889e5d
dkarchana07-ops <dkarchana07@gmail.com>
2026-09-12 18:34:17 +0530

    Complete Week 6 Category C practice problems

---
+++ src/main/java/week6/class_problems/StudentCollegeManager.java
@@ -0,0 +1,28 @@
+package week6.class_problems;
+
+class Student {
+    String name;
+    double attendance;
+    static String collegeName = "SRM Institute of Science and Technology";
+    static int studentCount = 0;
+
+    public Student(String name, double attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    public static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+}
+
+public class StudentCollegeManager {
+    public static void main(String[] args) {
+        new Student("Aarav", 88.5);
+        new Student("Diya", 92.0);
+
+        Student.printCollegeInfo();
+    }
+}
